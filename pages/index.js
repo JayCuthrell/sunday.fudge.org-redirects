981f8dd015ebd60f3c18640411fee9fdc859dca1
Jay Cuthrell <jay@cuthrell.com>
2023-06-23 21:15:53 -0400

    link to fudge.org

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Redirecting to fudge.org</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="We've moved!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          We've moved to <a href="https://fudge.org">fudge.org</a>
         </p>
       </main>
 
