03a8e0bab6582fc41e846c5e2a6976847a499947
Jay Cuthrell <jay@cuthrell.com>
2023-07-01 18:07:43 -0400

    Update index.js with youtube video

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,10 @@ export default function Home() {
         <p className="description">
           We've moved to <a href="https://fudge.org">fudge.org</a>
         </p>
+        <Header title="Here's how we did it!" />
+        <p className="description">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/CVfoImUj-NE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </p>
       </main>
 
       <Footer />
